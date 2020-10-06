594480d7d0223814d8eb2e925996d8a58df876e4
root <root@devalla.devalla>
2020-10-06 19:27:40 +0530

    updated

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import wx
 import wx.stc as stc
 import configparser
-import style, syntax
+import style, syntax, menu
 
 class win(wx.Frame):
 
@@ -30,7 +30,7 @@ class win(wx.Frame):
 		self.Bind(wx.EVT_TIMER, self.statlineinfo)
 		self.timer.Start(40)
 		self.Bind(wx.EVT_CLOSE, self.close)
-		self.Menu()
+		menu.Menu(self)
 		style.style(self)
 		self.init()
 
@@ -77,100 +77,87 @@ class win(wx.Frame):
 		col=self.control.GetColumn(self.control.GetCurrentPos())
 		self.statusbar.SetStatusText('     Line : %s  Col : %s' % (line+1, col),0)
 
-	def Menu(self):
-		self.rtm=configparser.RawConfigParser()
-		self.rtm.read('runtime')
-		li=dict(self.rtm.items('recent'))
-		for l in li:
-			li[l]=li[l].split('#',1)[0].strip()
-		globals().update(li)
+	def stylesetts(self,e):
+		settfile=open('style.cfg', 'r')
+		if self.control.GetModify()==True:
+			dlg = wx.MessageDialog(self, 'Do you want to Save the file??','Alert',wx.YES_NO | wx.ICON_QUESTION)
+			ans = dlg.ShowModal()
+			if ans==wx.ID_YES:
+				self.save(e)
+				self.control.SetValue(settfile.read())
+				self.filepath='style.cfg'
+				self.control.SetModified(False)
+				self.title()
+				self.statbar()
+			else:
+				self.control.SetValue(settfile.read())
+				self.filepath='style.cfg'
+				self.control.SetModified(False)
+				self.title()
+				self.statbar()
+		else:
+			self.control.SetValue(settfile.read())
+			self.filepath='style.cfg'
+			self.control.SetModified(False)
+			self.title()
+			self.statbar()
 
-		kb=configparser.RawConfigParser()
-		kb.read('keybinds.cfg')
-		par=dict(kb.items("keybinds"))
-		for p in self.par:
-		    self.par[p]=self.par[p].split("#",1)[0].strip()
+	# Menu Tools
+	def duplicateline(self,e):
+		self.control.SelectionDuplicate()
+	def delline(self,e):
+		self.control.LineDelete()
+	def lineup(self,e):
+		self.control.MoveSelectedLinesUp()
+	def linedown(self,e):
+		self.control.MoveSelectedLinesDown()
+	def uppercase(self,e):
+		self.control.UpperCase()
+	def lowercase(self,e):
+		self.control.LowerCase()
+	def unindent(self,e):
+		self.control.BackTab()
+	def indent(self,e):
+		self.control.Tab()
 
-		globals().update(self.par)
+	def find(self,e):
+		self.control.SearchInTarget('Wrap')
 
-		menu = wx.MenuBar()
-		self.SetMenuBar(menu)
+	def gotoline(self,e):
+		dlg = wx.NumberEntryDialog(self, 'Enter line no:','Go','Goto Line',1,1,9999)
+		ans=dlg.ShowModal()
+		n=dlg.GetValue()
+		if ans==wx.ID_OK:
+			self.control.GotoLine(n-1)
+		else:
+			pass
 
-		# File Menu
-		file = wx.Menu(); menu.Append(file, '&File');
-		new=file.Append(100,'&New'+'\t'+kb.get('keybinds','new'), 'Creates New File'); self.Bind(wx.EVT_MENU, self.new, new)
-		new.SetBitmap(wx.Bitmap("icons/new.ico"))
-		opens=file.Append(101,'&Open'+'\t'+kb.get('keybinds','open'), 'Browse File'); self.Bind(wx.EVT_MENU, self.opens, opens)
-		opens.SetBitmap(wx.Bitmap("icons/open.ico"))
-		recent=wx.Menu(); file.AppendSubMenu(recent,'Recent files')
-		r1=self.rtm.get('recent','r1');r2=self.rtm.get('recent','r2');r3=self.rtm.get('recent','r3')
-		r4=self.rtm.get('recent','r4');r5=self.rtm.get('recent','r5');r6=self.rtm.get('recent','r6')
-		if r1!='':
-			rec1=recent.Append(301,'01). '+r1); self.Bind(wx.EVT_MENU,self.openrecent1,rec1)
-		if r2!='':
-			rec2=recent.Append(302,'02). '+r2); self.Bind(wx.EVT_MENU,self.openrecent2,rec2)
-		if r3!='':
-			rec3=recent.Append(303,'03). '+r3); self.Bind(wx.EVT_MENU,self.openrecent3,rec3)
-		if r4!='':
-			rec4=recent.Append(304,'04). '+r4); self.Bind(wx.EVT_MENU,self.openrecent4,rec4)
-		if r5!='':
-			rec5=recent.Append(305,'05). '+r5); self.Bind(wx.EVT_MENU,self.openrecent5,rec5)
-		if r6!='':
-			rec6=recent.Append(306,'06). '+r6); self.Bind(wx.EVT_MENU,self.openrecent6,rec6)
-		recent.AppendSeparator()
-		remre=recent.Append(103,'Clear recents'); self.Bind(wx.EVT_MENU,self.rem_recents,remre)
-		# recent.SetBitmap(wx.Bitmap("icons/love2.ico"))
-		file.AppendSeparator()
-		save=file.Append(104,'&Save'+'\t'+kb.get('keybinds','save'), 'Save the Current file'); self.Bind(wx.EVT_MENU, self.save, save)
-		save.SetBitmap(wx.Bitmap("icons/save.ico"))
-		saveas=file.Append(105,'Save &as'+'\t'+kb.get('keybinds','saveas'), 'Save as other doc'); self.Bind(wx.EVT_MENU,self.saveas,saveas)
-		saveas.SetBitmap(wx.Bitmap("icons/saveas.ico"))
-		file.AppendSeparator()
-		reloadfile=file.Append(106,'Reload file')
-		reloadfile.SetBitmap(wx.Bitmap("icons/reload.ico"))
-		delfile=file.Append(107,'Delete file')
-		delfile.SetBitmap(wx.Bitmap("icons/delete.ico"))
-		file.AppendSeparator()
-		exit=file.Append(999,'&Exit'+'\t'+kb.get('keybinds','exit'), 'Exit from editor'); self.Bind(wx.EVT_MENU, self.close, exit)
-		exit.SetBitmap(wx.Bitmap("icons/exit2.ico"))
+	def comment(self,e):
+		c=self.control.GetCurrentPos()
+		l=self.control.GetCurrentLine()
+		e=self.control.GetLineLength(l)
+		self.control.GotoLine(l)
+		if self.fileext=='.py':
+			self.control.AddText('#')
+			self.control.GotoPos(c+1)
+		elif self.fileext=='.html':
+			self.control.AddText('<!-- ')
+			self.control.GotoPos(self.control.GetCurrentPos()+e)
+			self.control.AddText(' -->')
+			self.control.GotoPos(c+5)
+		else:
+			pass
+	def togglewrap(self,e):
+		if self.wrapmod.IsChecked()==True:
+			self.control.SetWrapMode(1)
+		else:
+			self.control.SetWrapMode(0)
 
-		# View Menu
-		view = wx.Menu(); menu.Append(view, '&View')
-		self.togtbar=view.AppendCheckItem(108,'Toolbar'); self.Bind(wx.EVT_MENU, self.toggletbar, self.togtbar)
-		# self.togtbar.SetBitmap(wx.Bitmap("icons/font.ico"))
-		self.wrapmod=view.AppendCheckItem(109,'&Wordwrap'+'\t'+kb.get('keybinds','wrap')); self.Bind(wx.EVT_MENU, self.togglewrap, self.wrapmod)
-		# self.wrapmode.SetBitmap(wx.Bitmap("icons/wrap.ico"))
-		self.fullscr=view.AppendCheckItem(110,'FullScreen'+'\t'+'F11'); self.Bind(wx.EVT_MENU, self.fullscreen, self.fullscr)
-
-		if self.cfg.getboolean('settings','wrap')==True:
-			self.wrapmod.Check()
-
-		search=view.Append(111,'&Find'+'\t'+kb.get('keybinds','find')); self.Bind(wx.EVT_MENU, self.find, search)
-		search.SetBitmap(wx.Bitmap("icons/find.ico"))
-
-		# Edit Menu
-		edit=wx.Menu(); menu.Append(edit,'&Edit')
-		line=wx.Menu(); edit.AppendSubMenu(line,'&Line')
-		goto=line.Append(112,'&Goto Line'+'\t'+kb.get('keybinds','goto')); self.Bind(wx.EVT_MENU, self.gotoline, goto)
-		line.AppendSeparator()
-		comm=line.Append(113, 'Comment'+'\t'+kb.get('keybinds','lcomment')); self.Bind(wx.EVT_MENU, self.comment, comm)
-		dupli=line.Append(114,'&Duplicate Line'+'\t'+kb.get('keybinds','linecopy')); self.Bind(wx.EVT_MENU, self.duplicateline, dupli)
-		delline=line.Append(115, 'Delete Line'+'\t'+kb.get('keybinds','delline')); self.Bind(wx.EVT_MENU, self.delline, delline)
-		line.AppendSeparator()
-		lineup=line.Append(116,'Move Line Up'+'\t'+kb.get('keybinds','lineup')); self.Bind(wx.EVT_MENU, self.lineup, lineup)
-		linedown=line.Append(117,'Move Line Down'+'\t'+kb.get('keybinds','linedown')); self.Bind(wx.EVT_MENU, self.linedown, linedown)
-		unindent=line.Append(118, 'Unindent Line'); self.Bind(wx.EVT_MENU,self.unindent,unindent)
-		indent=line.Append(119, 'Indent Line'); self.Bind(wx.EVT_MENU,self.indent,indent)
-		convcase=wx.Menu(); edit.AppendSubMenu(convcase,'&Convert Case')
-		upcase=convcase.Append(120, '&UpperCase'+'\t'+kb.get('keybinds','lowcase')); self.Bind(wx.EVT_MENU, self.uppercase, upcase)
-		locase=convcase.Append(121, '&LowerCase'+'\t'+kb.get('keybinds','upcase')); self.Bind(wx.EVT_MENU, self.lowercase, locase)
-
-		# Settings Menu
-		prefs=wx.Menu(); menu.Append(prefs, '&Preferences')
-		stylesetts=prefs.Append(122,'Settings'); self.Bind(wx.EVT_MENU, self.stylesetts, stylesetts)
-		stylesetts.SetBitmap(wx.Bitmap("icons/settings.ico"))
-		keysetts=prefs.Append(123, 'Key Bindings'); self.Bind(wx.EVT_MENU , self.keybinds, keysetts)
-		keysetts.SetBitmap(wx.Bitmap("icons/keys.ico"))
+	def fullscreen(self,e):
+		if self.fullscr.IsChecked()==True:
+			self.ShowFullScreen(True)
+		else:
+			self.ShowFullScreen(False)
 
 	def init(self):
 		# Wrap mode
@@ -210,89 +197,6 @@ class win(wx.Frame):
 			self.filepath='untitled'
 			self.fileext=''
 			self.SetTitle('Dev-Pad')
-
-	# Menu Tools
-	def duplicateline(self,e):
-		self.control.SelectionDuplicate()
-	def delline(self,e):
-		self.control.LineDelete()
-	def lineup(self,e):
-		self.control.MoveSelectedLinesUp()
-	def linedown(self,e):
-		self.control.MoveSelectedLinesDown()
-	def uppercase(self,e):
-		self.control.UpperCase()
-	def lowercase(self,e):
-		self.control.LowerCase()
-	def unindent(self,e):
-		self.control.BackTab()
-	def indent(self,e):
-		self.control.Tab()
-
-	def find(self,e):
-		self.control.SearchInTarget('Wrap')
-
-	def togglewrap(self,e):
-		if self.wrapmod.IsChecked()==True:
-			self.control.SetWrapMode(1)
-		else:
-			self.control.SetWrapMode(0)
-
-	def fullscreen(self,e):
-		if self.fullscr.IsChecked()==True:
-			self.ShowFullScreen(True)
-		else:
-			self.ShowFullScreen(False)
-
-	def gotoline(self,e):
-		dlg = wx.NumberEntryDialog(self, 'Enter line no:','Go','Goto Line',1,1,9999)
-		ans=dlg.ShowModal()
-		n=dlg.GetValue()
-		if ans==wx.ID_OK:
-			self.control.GotoLine(n-1)
-		else:
-			pass
-
-	def comment(self,e):
-		c=self.control.GetCurrentPos()
-		l=self.control.GetCurrentLine()
-		e=self.control.GetLineLength(l)
-		self.control.GotoLine(l)
-		if self.fileext=='.py':
-			self.control.AddText('#')
-			self.control.GotoPos(c+1)
-		elif self.fileext=='.html':
-			self.control.AddText('<!-- ')
-			self.control.GotoPos(self.control.GetCurrentPos()+e)
-			self.control.AddText(' -->')
-			self.control.GotoPos(c+5)
-		else:
-			pass
-
-	def stylesetts(self,e):
-		settfile=open('style.cfg', 'r')
-		if self.control.GetModify()==True:
-			dlg = wx.MessageDialog(self, 'Do you want to Save the file??','Alert',wx.YES_NO | wx.ICON_QUESTION)
-			ans = dlg.ShowModal()
-			if ans==wx.ID_YES:
-				self.save(e)
-				self.control.SetValue(settfile.read())
-				self.filepath='style.cfg'
-				self.control.SetModified(False)
-				self.title()
-				self.statbar()
-			else:
-				self.control.SetValue(settfile.read())
-				self.filepath='style.cfg'
-				self.control.SetModified(False)
-				self.title()
-				self.statbar()
-		else:
-			self.control.SetValue(settfile.read())
-			self.filepath='style.cfg'
-			self.control.SetModified(False)
-			self.title()
-			self.statbar()
 
 	def keybinds(self,e):
 		keys=open('keybinds.cfg', 'r')
@@ -462,7 +366,7 @@ class win(wx.Frame):
 			cfg.set('recent','r6',self.r6)
 			with open('runtime', 'w') as file:
 			    cfg.write(file)
-			self.Menu()
+			menu.Menu(self)
 
 	def rem_recents(self,e):
 		self.r1=self.filepath
@@ -484,7 +388,7 @@ class win(wx.Frame):
 		cfg.set('recent','r6',self.r6)
 		with open('runtime', 'w') as file:
 		    cfg.write(file)
-		self.Menu()
+		menu.Menu(self)
 
 	def untitled(self):
 		self.control.SetValue("")
